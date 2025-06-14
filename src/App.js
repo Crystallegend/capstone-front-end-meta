c3aa1d5c1fad2afd7900460a85c4d198a9f05320
Mufassir <duckyduck789@gmail.com>
2025-06-14 11:26:30 +0500

    HTML Sematic Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Header from "./Components/Header"
+import Nav from "./Components/Nav"
+import Main from "./Components/Main"
+import Footer from "./Components/Footer"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Components/Nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+    return (<nav>
+        <ul>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Reserve a Table</li>
+            <li>About</li>
+        </ul>
+    </nav>)
+}
+
+export default Nav;
